c4f2d0244d507fe5f967a7432fccc2cb45af43eb
Soneé John <sonee@flexibits.com>
2022-09-03 18:57:22 -0400

    Initial Commit

---
+++ EventKitDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  EventKitDemo
+//
+//  Created by Wolfgang on 9/3/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ EventKitDemo/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  EventKitDemo
+//
+//  Created by Wolfgang on 9/3/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
